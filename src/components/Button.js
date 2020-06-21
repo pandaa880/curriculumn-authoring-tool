16d93c2d70f6f6b2f6127799239c9f40363cb083
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-06-22 05:01:23 +0530

    add dynamic classname prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function Button({ children, type = "button", onClick = () => {}, ...props }) {
   return (
-    <button type={type} onClick={onClick} className="action-btn" {...props}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`${props.className && props.className}`}
+      {...props}
+    >
       {children}
     </button>
   );
