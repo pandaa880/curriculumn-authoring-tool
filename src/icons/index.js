7861fc525d8b4631c09de0a9271b1e96860e1268
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-06-22 05:03:02 +0530

    create icon components

---
+++ src/icons/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const move = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    className="action-btn__icon"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z" />
+  </svg>
+);
+export const outdent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    className="action-btn__icon"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z" />
+  </svg>
+);
+
+export const indent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    className="action-btn__icon action-btn__icon--rotate"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z" />
+  </svg>
+);
+
+export const deleteIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    className="action-btn__icon"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+  </svg>
+);
