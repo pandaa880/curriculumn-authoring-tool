ec8e89c9203d3e9635a7dc6deb135e1d668200d4
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-06-21 23:58:52 +0530

    add action constants

---
+++ src/constants/actions.js
@@ -0,0 +1,7 @@
+export const TRANSFORM = "transform";
+export const ADD = "add";
+export const INDENT = "indent";
+export const OUTDENT = "outdent";
+export const UPDATE_ROW_VALUE = "update_row_value";
+export const INITIALIZE = "initialize";
+export const DELETE = "delete";
